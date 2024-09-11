35b4290d27110c73fcd066f1acdf6c6be3d21663
Simon <randyhaha@gmail.com>
2024-09-11 11:58:35 +0800

    update

--- README.md
+++ README.md
@@ -55,3 +55,43 @@ Copy to where you want and use for good.
 
 
 ./mdb-json WSUJ.MDB tab71 | iconv -f utf-8 -t cp1252 | iconv -f BIG5-2003 -t utf-8 > raw-tab71.txt ; sed '1s/^/[/;$!s/$/,/;$s/$/]/' raw-tab71.txt > raw-tab71.json
+
+
+"C:\Program Files\Git\usr\bin\iconv.exe"
+
+
+
+---
+
+範例的 bat
+
+@echo off
+
+CD C:\Users\simon\ys-dashboard\downloadcsv
+
+
+
+
+"C:\Program Files\Git\mingw64\bin\wget.exe" "https://docs.google.com/spreadsheets/d/e/2PACX-1vQWtQXnkGEqIvkfQtzJjfG04aEoBEEX9UrBnuAYTc7o2r7PjeDahhI2lbKx_IFCcyDo1GwrUUYVCucQ/pub?gid=0&single=true&output=csv" -O "allcars.csv"
+
+
+"C:\Program Files\Git\mingw64\bin\wget.exe" "https://docs.google.com/spreadsheets/d/e/2PACX-1vSGooecTQaPb3-JrGJUvZWVSaSQn2bUwfwT30PNqhOk0iiTmFkACRQmcfyF5xLqyMSFFI9seIZgj006/pub?gid=0&single=true&output=csv" -O "carinsurance.csv"
+
+
+"C:\Program Files\Git\mingw64\bin\wget.exe" "https://docs.google.com/spreadsheets/d/e/2PACX-1vRlZMp9eMH0r7zva4GFZRUDxmrREwULZGVPU_icxpH2SYK4ZA5hIbSTV1537IiczSX94fiXk0MKIPYB/pub?gid=0&single=true&output=csv" -O "checkdesk.csv"
+
+
+"C:\Program Files\Git\mingw64\bin\wget.exe" "https://docs.google.com/spreadsheets/d/e/2PACX-1vT0qnaulrS0_Cln5fNWx70xh8Azb2IrVCXPWdWZVljy043jNGQfnvUKBQcB1eGjLQ-VncXIz0hmIJRh/pub?gid=751335063&single=true&output=csv" -O "checkinvoice.csv"
+
+
+"C:\Program Files\Git\mingw64\bin\wget.exe" "https://docs.google.com/spreadsheets/d/e/2PACX-1vRhymumykVDxO_WmB8jKj_t1I14faufVnzHAw9rxMzYiXpF1CuV1AWeuwixrnmnOeL4Y7_xt9fiAqrw/pub?gid=1868011144&single=true&output=csv" -O "checkloan.csv"
+
+
+"C:\Program Files\Git\mingw64\bin\wget.exe" "https://docs.google.com/spreadsheets/d/e/2PACX-1vSpquKHN5drHnFTAASuKaHnIa7jfTApOQ9Je4dVSZmGQmgHbMi7LrcRZF7VC1YyMQJbOOqD7bgRTh5O/pub?gid=0&single=true&output=csv" -O "generalcontacts.csv"
+
+
+COPY *.csv "../src/assets/csv"
+
+
+
+DIR "../src/assets/csv" > "../src/assets/csv/updates.log"
--- export-db.bat
+++ export-db.bat
@@ -1,7 +1,57 @@
 @echo off
 
-CD C:\Users\randy\mdbtools-win
 
-"C:\Users\randy\mdbtools-win\mdb-json.exe" WSUJ.MDB tab76 | iconv -f utf-8 -t cp1252 | iconv -f BIG5-2003 -t utf-8 > raw-tab76.txt ; sed '1s/^/[/;$!s/$/,/;$s/$/]/' raw-tab76.txt > raw-tab76.json
+@REM WSUJ 一定要放英文路徑名稱的資料夾才找得到，好煩
+@REM 要先修改筠電腦上的微笑出帳名稱 先改為英文名字
 
-./mdb-json WSUJ.MDB tab50 > raw-tab50.txt ; sed '1s/^/[/;$!s/$/,/;$s/$/]/' raw-tab50.txt > raw-tab50.json ; ./mdb-json WSUJ.MDB tab71 | iconv -f utf-8 -t cp1252 | iconv -f BIG5-2003 -t utf-8 > raw-tab71.txt ; sed '1s/^/[/;$!s/$/,/;$s/$/]/' raw-tab71.txt > raw-tab71.json ; ./mdb-json WSUJ.MDB tab73 | iconv -f utf-8 -t cp1252 | iconv -f BIG5-2003 -t utf-8 > raw-tab73.txt ; sed '1s/^/[/;$!s/$/,/;$s/$/]/' raw-tab73.txt > raw-tab73.json ; ./mdb-json WSUJ.MDB tab75 | iconv -f utf-8 -t cp1252 | iconv -f BIG5-2003 -t utf-8 > raw-tab75.txt ; sed '1s/^/[/;$!s/$/,/;$s/$/]/' raw-tab75.txt > raw-tab75.json ; ./mdb-json WSUJ.MDB tab76 | iconv -f utf-8 -t cp1252 | iconv -f BIG5-2003 -t utf-8 > raw-tab76.txt ; sed '1s/^/[/;$!s/$/,/;$s/$/]/' raw-tab76.txt > raw-tab76.json
+@REM 先用 mdbtools-win.exe , export WSUJ.MDB 內的所有需要的 table, 然後轉成 5 個 json 檔案
+
+
+"C:\Users\randy\mdbtools-win\mdb-json.exe" "C:\Users\randy\smile-data\WSUJ.MDB" tab50 > raw-tab50.txt
+
+"C:\Program Files\Git\usr\bin\sed.exe" "1s/^/[/;$!s/$/,/;$s/$/]/" raw-tab50.txt > raw-tab50.json
+
+
+
+"C:\Users\randy\mdbtools-win\mdb-json.exe" "C:\Users\randy\smile-data\WSUJ.MDB" tab71 | "C:\Program Files\Git\usr\bin\iconv.exe" -f "utf-8" -t "cp1252" | "C:\Program Files\Git\usr\bin\iconv.exe" -f "BIG5-2003" -t "utf-8" > raw-tab71.txt 
+
+"C:\Program Files\Git\usr\bin\sed.exe" "1s/^/[/;$!s/$/,/;$s/$/]/" raw-tab71.txt > raw-tab71.json
+
+
+
+"C:\Users\randy\mdbtools-win\mdb-json.exe" "C:\Users\randy\smile-data\WSUJ.MDB" tab73 | "C:\Program Files\Git\usr\bin\iconv.exe" -f "utf-8" -t "cp1252" | "C:\Program Files\Git\usr\bin\iconv.exe" -f "BIG5-2003" -t "utf-8" > raw-tab73.txt 
+
+"C:\Program Files\Git\usr\bin\sed.exe" "1s/^/[/;$!s/$/,/;$s/$/]/" raw-tab73.txt > raw-tab73.json
+
+
+
+"C:\Users\randy\mdbtools-win\mdb-json.exe" "C:\Users\randy\smile-data\WSUJ.MDB" tab75 | "C:\Program Files\Git\usr\bin\iconv.exe" -f "utf-8" -t "cp1252" | "C:\Program Files\Git\usr\bin\iconv.exe" -f "BIG5-2003" -t "utf-8" > raw-tab75.txt 
+
+"C:\Program Files\Git\usr\bin\sed.exe" "1s/^/[/;$!s/$/,/;$s/$/]/" raw-tab75.txt > raw-tab75.json
+
+
+
+"C:\Users\randy\mdbtools-win\mdb-json.exe" "C:\Users\randy\smile-data\WSUJ.MDB" tab76 | "C:\Program Files\Git\usr\bin\iconv.exe" -f "utf-8" -t "cp1252" | "C:\Program Files\Git\usr\bin\iconv.exe" -f "BIG5-2003" -t "utf-8" > raw-tab76.txt 
+
+"C:\Program Files\Git\usr\bin\sed.exe" "1s/^/[/;$!s/$/,/;$s/$/]/" raw-tab76.txt > raw-tab76.json
+
+
+@REM 將所有產出的 json copy 到 smile project 內的資料夾
+
+
+COPY *.json "C:\Users\randy\smile-project"
+
+@REM 切換到 smile project 目錄，準備篩選月份，及整合成一個大的 json 檔案
+
+CD "C:\Users\randy\smile-project"
+
+"C:\Users\randy\AppData\Local\Programs\Python\Python312\python.exe" filter_json.py
+
+
+@REM 複製產出的檔案到查詢系統內
+
+
+COPY output-smiledata.json "C:\Users\randy\ys-dashboard-test\src\assets\csv\"
+
+
+pause
